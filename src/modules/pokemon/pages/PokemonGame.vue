d7cb1f8021ff006f917c817c3debdd21f170c05e
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-06 13:33:34 +0200

    feat: add Pokemon game functionality with options and picture components

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,17 +1,53 @@
 <template>
-  <div v-if="false" class="flex flex-col justify-center items-center h-screen w-screen">
+  <section
+    v-if="isLoading || randomPokemon?.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
     <h1 class="text-3xl">Espere por favor</h1>
-    <h3 class="animate-pulse">Cargando Pokemons</h3>
-  </div>
+    <h3 class="animate-pulse">Cargando Pokémons</h3>
+  </section>
 
-  <section v-else class="flex flex-col justify-center items-center h-screen w-screen">
-    <h1 class="text-3xl">¿Quién es este Pokémon?</h1>
-    <PokemonPicture />
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">¿Quién es este Pokémon?</h1>
+
+    <div class="h-20">
+      <button
+        v-if="gameStatus !== GameStatus.Playing"
+        @click="getNextRound(4)"
+        class="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 transition-all"
+      >
+        ¿Jugar de nuevo?
+      </button>
+    </div>
+
+    <!-- Pokemon Picture -->
+    <PokemonPicture
+      :pokemon-id="randomPokemon?.id || 0"
+      :show-pokemon="gameStatus !== GameStatus.Playing"
+    />
+
+    <!-- Pokemon Options -->
+    <PokemonOptions
+      :options="options"
+      :block-selection="gameStatus !== GameStatus.Playing"
+      :correct-answer="randomPokemon?.id || 0"
+      @selected-option="checkAnswer"
+    />
   </section>
 </template>
 
 <script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
-</script>
+import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
 
-<style lang="scss" scoped></style>
+const {
+  randomPokemon,
+  isLoading,
+  gameStatus,
+  pokemonOptions: options,
+  checkAnswer,
+  getNextRound,
+} = usePokemonGame();
+</script>
